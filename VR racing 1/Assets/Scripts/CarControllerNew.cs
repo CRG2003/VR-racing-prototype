4d4cf9822644777887010c35a584cbf42b9cb74a
CRG2003 <jakecraig50@gmail.com>
2024-02-26 09:09:10 +0000

    Initialised car settings for control

--- VR racing 1/Assets/Scripts/CarControllerNew.cs
+++ VR racing 1/Assets/Scripts/CarControllerNew.cs
@@ -7,8 +7,46 @@ public class CarControllerNew : MonoBehaviour
     public Transform car;
     public Transform wfl, wfr, wbl, wbr;
 
+    public float curTorque = 100f, powerShift = 100f, motorRPM = 0f;
+    public bool brake, shift, neutral = true, backwards = false;
 
-    void Start()
+    private float steer = 0f, accel = 0f, lastSpeed = -10f;
+    private float shiftTime = 0f, shiftDelay = 0f
+    private bool shiftMotor;
+
+    public class wheelSetting{
+        public float radius = .4f, weight = 1000f, dist = 0f;
+    }
+
+
+    public class carSettings{
+        public Transform steer;
+        public Ground[] ground;
+
+        // suspension
+        public float springs = 25000f, dampers = 1500f;
+
+        // power
+        public float carPower = 120f, shiftPower = 150f, brakePower = 8000f;
+
+        // RPM
+        public float shiftDownRPM = 1500f, shiftUpRPM = 2500f, idleRPM = 500f;
+
+        public float maxSteerAngle = 25f;
+
+        public float[] gears = {-10f, 9f, 6f, 4.5f, 3, 2.5f};
+
+        // speed limits
+        public float limitForward = 220f, limitBackwards = 60f;
+    }
+
+
+    public class Ground{
+        public string tag = "ground";
+        public bool grounded = false;
+    }
+
+    void Awake()
     {
         
     }
